8ec31820a34890afd31129480e5c8114c257a18a
Virendra <patelvirendra62@gmail.com>
2020-05-10 23:38:04 +0530

    Navigation Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' , component : HomeComponent}, 
+  {path : 'home' , component : HomeComponent},
+  {path : 'orders' , component : UserOrdersComponent}, 
+  {path : 'cart' , component : CartComponent}, 
+  {path : 'login' , component : LoginComponent}, 
+  {path : 'signup' , component : SignupComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { StoreComponent } from './components/store/store.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    StoreComponent,
+    FilterComponent,
+    ProductCardComponent,
+    UserOrdersComponent,
+    CartComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
