46d72123e711be32a7b4a3c5e945a7d161451506
Virendra <patelvirendra62@gmail.com>
2020-05-11 17:25:43 +0530

    login desgin

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  form : HTMLFormElement
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(event : Event){
+    event.preventDefault();
+    this.form = <HTMLFormElement>event.target
+    this.readFormValues();
+  }
+
+  readFormValues(){
+    let email = (<HTMLInputElement>
+                  this.form.elements.namedItem('email')).value
+    let password = (<HTMLInputElement>
+                    this.form.elements.namedItem('password')).value;
+    
+    let creadentials = {
+      email , password
+    }
+
+    console.log(creadentials);
+    
+    
+
+  }
 }
