2a197c0871d9ba54db4f3d6ce56b09e6ff56fbd0
Virendra <patelvirendra62@gmail.com>
2020-05-15 12:02:36 +0530

    unsubscribing

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { Product } from 'src/app/models/products';
 import { ProductService } from 'src/app/services/product/product.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 interface CartItem {
@@ -20,6 +20,7 @@ export class CartComponent implements OnInit {
   cart;
   total= 0
   cartItems: CartItem[]  = [];
+  cartSubscription : Subscription ;
 
   constructor(private cartService: CartService, private productService: ProductService) { }
 
@@ -27,20 +28,21 @@ export class CartComponent implements OnInit {
       this.subcribeCart()
   }
 
-
+  ngOnDestroy(): void {
+    this.cartSubscription.unsubscribe()
+}
   subcribeCart() {
     let total = 0;
-    this.cartService.cartObservable.subscribe(
+   this.cartSubscription =  this.cartService.cartObservable.subscribe(
       {
         next: (cart) => {
-          
+          console.log(cart);
           let observables = []
           total = 0;
           if(Object.keys(cart).length == 0){
             this.cartItems = [] 
           }
           for (let id in cart) {
-            console.log(id);
             observables.push(
               this.productService.getProductById(id)
                 .pipe(
