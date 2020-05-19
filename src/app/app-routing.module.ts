5239cc492cdef9027c31130af8734c95bd1aed70
Virendra <patelvirendra62@gmail.com>
2020-05-19 11:44:05 +0530

    modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,81 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from 'src/app/core/components/home/home.component';
-import { UserOrdersComponent } from 'src/app/user/components/user-orders/user-orders.component';
-import { CartComponent } from 'src/app/user/components/cart/cart.component';
-import { SignupComponent } from 'src/app/core/components/signup/signup.component';
-import { LoginComponent } from 'src/app/core/components/login/login.component';
-import { AdminHomeComponent } from 'src/app/admin/components/admin/admin-home/admin-home.component';
-import { AdminDashboardComponent } from 'src/app/admin/components/admin/admin-dashboard/admin-dashboard.component';
-import { AdminProductsComponent } from 'src/app/admin/components/admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from 'src/app/admin/components/admin/admin-orders/admin-orders.component';
-import { AdminNewProductComponent } from 'src/app/admin/components/admin/admin-new-product/admin-new-product.component';
-import { AdminCustomersComponent } from 'src/app/admin/components/admin/admin-customers/admin-customers.component';
-import { UserAuthGuardService } from 'src/app/shared/auth-guard/user-auth-guard.service';
-import { AdminAuthGuardService } from 'src/app/shared/auth-guard/admin-auth-guard.service';
+import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    canActivate: [UserAuthGuardService],
-    component: HomeComponent
-  },
-  {
-    path: 'home',
-    canActivate: [UserAuthGuardService],
-    component: HomeComponent
-  },
-  {
-    path: 'orders',
-    canActivate: [UserAuthGuardService],
-    component: UserOrdersComponent
-  },
-  {
-    path: 'cart',
-    canActivate: [UserAuthGuardService],
-    component: CartComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  },
-  {
-    path: 'admin',
-    component: AdminHomeComponent,
-    canActivate: [UserAuthGuardService , AdminAuthGuardService ],
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: AdminDashboardComponent
-      },
-      {
-        path: 'orders',
-        component: AdminOrdersComponent
-      },
-      {
-        path: 'products',
-        component: AdminProductsComponent
-      },
-      {
-        path: 'new-product',
-        component: AdminNewProductComponent
-      },
-      {
-        path: 'customers',
-        component: AdminCustomersComponent
-      },
-    ]
-  },
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,62 +4,29 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChartsModule } from 'ng2-charts';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { AdminCustomersComponent } from 'src/app/admin/components/admin/admin-customers/admin-customers.component';
-import {
-  AdminDashboardCardComponent,
-} from 'src/app/admin/components/admin/admin-dashboard-card/admin-dashboard-card.component';
-import { AdminDashboardComponent } from 'src/app/admin/components/admin/admin-dashboard/admin-dashboard.component';
-import { AdminHomeComponent } from 'src/app/admin/components/admin/admin-home/admin-home.component';
-import { AdminNewProductComponent } from 'src/app/admin/components/admin/admin-new-product/admin-new-product.component';
-import { AdminOrdersComponent } from 'src/app/admin/components/admin/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from 'src/app/admin/components/admin/admin-products/admin-products.component';
-import { FooterComponent } from 'src/app/core/components/footer/footer.component';
-import { HeaderComponent } from 'src/app/core/components/header/header.component';
-import { HomeComponent } from 'src/app/core/components/home/home.component';
-import { LoginComponent } from 'src/app/core/components/login/login.component';
-import { SignupComponent } from 'src/app/core/components/signup/signup.component';
 import { HeaderInterceptorService } from 'src/app/shared/interceptor/header-interceptor.service';
-import { CartComponent } from 'src/app/user/components/cart/cart.component';
-import { FilterComponent } from 'src/app/user/components/filter/filter.component';
-import { ProductCardComponent } from 'src/app/user/components/product-card/product-card.component';
-import { ProductQuantityComponent } from 'src/app/user/components/product-quantity/product-quantity.component';
-import { StoreComponent } from 'src/app/user/components/store/store.component';
-import { UserOrdersComponent } from 'src/app/user/components/user-orders/user-orders.component';
 
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    StoreComponent,
-    FilterComponent,
-    ProductCardComponent,
-    UserOrdersComponent,
-    CartComponent,
-    LoginComponent,
-    SignupComponent,
-    ProductQuantityComponent,
-    AdminHomeComponent,
-    AdminDashboardComponent,
-    AdminOrdersComponent,
-    AdminProductsComponent,
-    AdminNewProductComponent,
-    AdminCustomersComponent,
-    AdminDashboardCardComponent
   ],
   imports: [
     BrowserModule,
+    UserModule,
+    AdminModule,
+    CoreModule,
     AppRoutingModule , 
     HttpClientModule,
     MatSnackBarModule,
     MatProgressBarModule,
-    ModalModule.forRoot(), ChartsModule, BrowserAnimationsModule
+    ModalModule.forRoot(),  BrowserAnimationsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptorService, multi: true },
