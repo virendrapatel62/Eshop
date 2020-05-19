103bc25bb6b6d1805319417c1af34a63d9007d8a
Virendra <patelvirendra62@gmail.com>
2020-05-19 07:21:47 +0530

    Update progress.service.ts

--- src/app/services/progress/progress.service.ts
+++ src/app/services/progress/progress.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ProgressService {
-  private _loaderObservable : BehaviorSubject<boolean>  = new BehaviorSubject(true); 
+  private _loaderObservable : BehaviorSubject<boolean>  = new BehaviorSubject(false); 
   constructor() { }
   
   public get loaderObservable() {
