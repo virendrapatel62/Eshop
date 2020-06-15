0dda966cd686fbb2074006f78440f215e2126195
Virendra <patelvirendra62@gmail.com>
2020-06-15 17:20:49 +0530

    ..

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -9,69 +9,68 @@ import { Router, ActivatedRoute, NavigationEnd, NavigationStart } from '@angular
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  numberOfItems : number = 0 ;
+  numberOfItems: number = 0;
   isLoggedIn = false;
   isCollapsed = false;
   isAdminUrl = false;
   isAdmin$
-  constructor(private cartService : CartService ,
-    private router : Router, 
-    private userService : UserService) { 
+  constructor(private cartService: CartService,
+    private router: Router,
+    private userService: UserService) {
 
 
-        router.events.subscribe({
-          next : (event)=>{
-            console.log(event);
-            
-            if(event instanceof NavigationStart)
-            {
-              let url = (<NavigationStart>event).url
-                this.isAdminUrl = url.includes('/admin')
-            }
-          }
-        })
-    }
+    router.events.subscribe({
+      next: (event) => {
+        console.log(event);
+
+        if (event instanceof NavigationStart) {
+          let url = (<NavigationStart>event).url
+          this.isAdminUrl = url.includes('/admin')
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
 
 
     this.cartService.cartObservable.subscribe({
-      next : (cart)=>{
+      next: (cart) => {
         console.log(cart);
         this.numberOfItems = Object.keys(cart).length
-        
+
       }
     })
 
 
-    
+
 
     this.userService.loginObservable.subscribe(
       {
-        next : ()=>{
+        next: () => {
 
           let token = this.userService.getToken();
-          if(token!= ''){
+          if (token != '') {
             this.cheakAdmin()
             this.isLoggedIn = true;
-          }else{
+          } else {
             this.isLoggedIn = false;
           }
           console.log(this.isLoggedIn);
-          
+
         }
       }
     )
   }
 
-  cheakAdmin(){
+  cheakAdmin() {
     // cheak user is admin or not 
     this.isAdmin$ = this.userService.isAdmin()
 
   }
 
 
-  logout(){
+  logout() {
     // alert('')
     this.userService.logout()
     this.router.navigate(['login'])
